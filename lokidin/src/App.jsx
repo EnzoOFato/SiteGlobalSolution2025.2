5eaedd9e42f9fa47c1db63d7a0f876aad16e9477
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-11-14 00:02:44 -0300

    Feat: Footer

--- lokidin/src/App.jsx
+++ lokidin/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className="min-h-screen flex items-center flex-col bg-amber-50">
         <Header></Header>
+        <Footer></Footer>
       </div>
     </>
   )
---
+++ lokidin/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <div className="w-full flex justify-center items-center py-5 bg-white shadow-lg">
+            <p text-sm text-gray-800>Todos direitos reservados © 2025 Lokidin</p>
+        </div>
+    )
+}
