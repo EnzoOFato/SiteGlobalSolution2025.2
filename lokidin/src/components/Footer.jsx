22573e34503ab7486f8eb8aaae971bdb5043eaa5
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-11-14 00:26:22 -0300

    Fix: Resposividade-Header

--- lokidin/src/components/Footer.jsx
+++ lokidin/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return (
-        <div className="w-full flex justify-center items-center py-5 bg-white shadow-lg">
-            <p text-sm text-gray-800>Todos direitos reservados © 2025 Lokidin</p>
+        <div className="w-full flex justify-center items-center py-5 bg-white shadow-lg mt-100">
+            <p className="text-sm text-gray-600">Todos direitos reservados © 2025 Lokidin</p>
         </div>
     )
 }
--- lokidin/src/components/Header.jsx
+++ lokidin/src/components/Header.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react"
 import logo from "../assets/imgs/logo.png"
+import menu from "../assets/imgs/menu.png"
 
 export default function Header(){
+
+    const [aberto, setAberto] = useState(false)
+
     return (
         <div className="w-full bg-white grid grid-cols-2 shadow-lg py-5">
             <div className="flex items-center justify-center">
                 <a href="#"><img src={logo} alt="Logo" className="w-15 h-15"/></a>
             </div>
-            <div className="flex justify-start items-center gap-10">
-                <a className="text-gray-800 text-lg" href="#">Home</a>
-                <a className="text-gray-800 text-lg" href="#">Minha Rede</a>
-                <a className="text-gray-800 text-lg" href="#">Vagas</a>
+            <div className="sm:flex justify-start items-center gap-10 hidden">
+                <a className="text-gray-800 text-lg hover:text-rose-600 transition duration-200 ease-in-out" href="#">Home</a>
+                <a className="text-gray-800 text-lg hover:text-rose-600 transition duration-200 ease-in-out" href="#">Minha Rede</a>
+                <a className="text-gray-800 text-lg hover:text-rose-600 transition duration-200 ease-in-out" href="#">Vagas</a>
+            </div>
+            <div className="sm:hidden flex items-end justify-end mr-10">
+                <button onClick={() => setAberto(!aberto)}><img src={menu} alt="Menu" className="w-15 h-15"/></button>
+            </div>
+            <div className={aberto ? "flex flex-col w-full items-center absolute top-25 bg-white" : "hidden"}>
+                <a className="text-gray-800 text-lg text-center py-4 shadow-sm w-full" href="#">Home</a>
+                <a className="text-gray-800 text-lg text-center py-4 shadow-sm w-full" href="#">Minha Rede</a>
+                <a className="text-gray-800 text-lg text-center py-4 shadow-sm w-full" href="#">Vagas</a>
             </div>
         </div>
     )
