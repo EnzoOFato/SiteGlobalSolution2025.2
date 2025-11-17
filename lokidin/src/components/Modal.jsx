44731ece3f4f5d587f7497094e2179c669013154
João <joaovictorrmattos@gmail.com>
2025-11-16 23:03:41 -0300

    AjusteResponsividade

--- lokidin/src/components/Modal.jsx
+++ lokidin/src/components/Modal.jsx
@@ -1,9 +1,28 @@
-export default function Modal({ aberto, fechar, children}){
+export default function Modal({ aberto, fechar, children }) {
     return (
-        <div onClick={fechar} className={`fixed inset-0 flex justify-center items-center ${aberto ? "visible bg-black/20" : "invisible"}`}>
-            <div onClick={(e) => e.stopPropagation()} className={`bg-white rounded-xl shadow p-6 transition-all ${aberto ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}>
+        <div
+            onClick={fechar}
+            className={`fixed inset-0 z-50 flex justify-center items-center 
+            ${aberto ? "visible bg-black/40 backdrop-blur-sm" : "invisible"}`}
+        >
+            <div
+                onClick={(e) => e.stopPropagation()}
+                className={`
+                    relative bg-white rounded-xl shadow-lg p-6 
+                    w-[95%] max-w-2xl max-h-[90vh] overflow-y-auto
+                    transition-all 
+                    ${aberto ? "scale-100 opacity-100" : "scale-110 opacity-0"}
+                `}
+            >
+                <button
+                    onClick={fechar}
+                    className="absolute top-3 right-3 text-4xl font-extrabold leading-nonetext-black hover:text-gray-700select-none"
+                >
+                    ×
+                </button>
+
                 {children}
             </div>
         </div>
-    )
+    );
 }
